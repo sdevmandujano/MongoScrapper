cfe55fc2750f8731d7358875a51249dd8a528cee
sdevmandujano <zmandujano01@gmail.com>
2018-08-26 11:48:46 -0500

    commit

---
+++ config/routes.js
@@ -0,0 +1,76 @@
+
+    
+var scrape = require("../scripts/scrape");
+
+var headlinesController = require(".../controllers/headlines");
+var notesController = require("../controllers/notes");
+
+
+//This renders the handlebars templates
+
+module.exports = function(router) {
+
+    router.get("/", function(req, res) {
+        res.render("home");
+
+   }); 
+
+router.get("/saved", function(req, res) {
+    res.render("saved");
+   });
+
+router.get("/api/fetch", function(req, res) {
+    headlinesController.fetch(function(err, docs) {
+        if (!docs || docs.insertedCount === 0) {
+            res.json( {
+                message: "No new articles today. check back tomorrow!"
+            });
+        }
+        else {
+            res.json({
+                message: "Added " + docs.insertedCount + "new articles!"
+        });
+    }
+
+});
+});
+router.get("/api/headlines", function(req, res) {
+    var query = {};
+    if (req.query.saved) {
+        query = req.query;
+    }
+
+    headlinesController.get(query, function(data) {
+        res.json(data);
+    });
+});
+
+router.delete("/api/headlines/:id", function(req, res){
+    var query = {};
+    query._id = req.params.id;
+    headlinesController.delete(query, function(err, data){
+
+    }); 
+
+});
+
+router.patch("/api/headline_id?", function(req, res) {
+    headlinesController.update(req.body, function(err, data) {
+        res.json(data);
+    });
+});
+router.get("/api/notes/:headline_id?", function(req, res){
+    var query = {};
+    if (req.params.headline_id) {
+        query._id = req.params.headline_id;
+    }
+    notesController.get(query, function(err, data) {
+        res.json(data);
+    });
+});
+router.delete("/api/notes", function(req, res){
+    notesController.save(req.body, function(data){
+        res.json(data);
+    });
+});
+}
